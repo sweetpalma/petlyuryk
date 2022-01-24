839b935092fea6814c081038cae6a5d54a00cd2a
Paul Marlow <paul.marlov@wagestream.com>
2022-01-24 20:40:33 +0200

    Reimplemented retarded joke module.

--- src/modules/module-joke.ts
+++ src/modules/module-joke.ts
@@ -3,33 +3,56 @@ import { BotkitExtended } from '../types';
 import { regexPhrase } from '../utils';
 
 
-const TRIGGER_NI = (
-	regexPhrase(/ні$/i)
-);
-
-
-const TRIGGER_NE = (
-	regexPhrase(/нє$/i)
-);
-
-
-const RESPONSE_NI = [
-	'Рука в гавні',
-	'Привіт',
+const RETARDED_JOKES: Array<[RegExp[], string[]]> = [
+	[
+		[
+			regexPhrase(/ні$/i),
+		],
+		[
+			'Рука в гавні',
+			'Привіт',
+		],
+	],
+	[
+		[
+			regexPhrase(/нє$/i),
+		],
+		[
+			'Рука в гавнє',
+		],
+	],
+	[
+		[
+			regexPhrase(/да$/i),
+		],
+		[
+			'Хуй на',
+			'Пізда',
+		],
+	],
+	[
+		[
+			regexPhrase(/нет$/i),
+		],
+		[
+			'Підора атвєт',
+		],
+	],
+	[
+		[
+			regexPhrase(/ти$/i),
+		],
+		[
+			'Тобі насрали в рот коти.',
+		],
+	],
 ];
 
 
-
-const RESPONSE_NE = (
-	'Рука в гавнє'
-);
-
-
 export default (controller: BotkitExtended) => {
-	controller.hears(TRIGGER_NI, 'message', async (bot, msg) => {
-		await bot.say(sample(RESPONSE_NI)!);
-	});
-	controller.hears(TRIGGER_NE, 'message', async (bot, msg) => {
-		await bot.say(RESPONSE_NE);
+	RETARDED_JOKES.forEach(([ triggers, responses ]) => {
+		controller.hears(triggers, 'message', async (bot, msg) => {
+			await bot.say(sample(responses)!);
+		});
 	});
 };
